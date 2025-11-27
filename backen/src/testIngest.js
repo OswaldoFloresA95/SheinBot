6a22199fb9ba8ccb8590830998608b53f203161f
Mario Diaz <mariod1452@gmail.com>
2025-11-27 08:52:01 -0600

    ingest

--- backen/src/testIngest.js
+++ backen/src/testIngest.js
@@ -25,7 +25,7 @@ async function main() {
     // "https://codeso.mx/plan-sonora/",
     // "https://www.gob.mx/conavi/acciones-y-programas/programa-de-vivienda-para-el-bienestar-2025",
 
-    "https://www.proyectosmexico.gob.mx/proyecto_inversion/polos-de-desarrollo-economico-para-el-bienestar/",
+    //"https://www.proyectosmexico.gob.mx/proyecto_inversion/polos-de-desarrollo-economico-para-el-bienestar/",
     "https://www.proyectosmexico.gob.mx/proyecto_inversion/1001-podecobi-seybaplaya-i-campeche/",
     "https://www.proyectosmexico.gob.mx/proyecto_inversion/1002-podecobi-san-jeronimo-chihuahua/",
     "https://www.proyectosmexico.gob.mx/proyecto_inversion/1003-podecobi-chetumal-quintana-roo/",
